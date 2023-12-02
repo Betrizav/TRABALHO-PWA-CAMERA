b27b7555481fa04cd92e2a61ecb03c37b845e195
Betrizav <beatriz.vieira@estudante.ifms.edu.br>
2023-12-01 21:10:11 -0400

    ba

--- js/db.js
+++ js/db.js
@@ -4,16 +4,23 @@ let db;
 
 async function createDB() {
   try{
-    db = await openDB('banco', 1,) {
+    db = await openDB('banco', 1, {
         upgrade ( db, oldVersion, newVersion, transaction){
             switch (oldVersion) {
                 case 0:
                 case 1:
-                    const
+                    const store = db.createObjectStore('pessoas', {
+                        keyPath: 'nome'
+                    });
+                    store.createIndex('id', 'id');
+                    showResult("Banco de dados criado!");
             }
         }
-    }
+    });
+    showResult("Banco de dados aberto.");
+  } catch (e) {
+    showResult("Erro ao criar o banco de dados:" + e.massage)
   }
 
-}
 
+}
