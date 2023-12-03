b5fbf8c64f5a56af42d2c23a3527b9c8e85db82b
Betrizav <beatriz.vieira@estudante.ifms.edu.br>
2023-12-02 23:47:17 -0400

    n adianta

--- js/db.js
+++ js/db.js
@@ -25,41 +25,9 @@ async function createDB() {
 
 window.addEventListener('DOMContentLoaded', async event =>{
   criarDB();
-  document.getElementById('camera').addEventListener('click', cadastrarFlor);
-  document.getElementById('listar').addEventListener('click', buscarFlor);
+  document.getElementById('botaoCadastro').addEventListener('click', cadastrarFlor);
+  document.getElementById('botaoMostrar').addEventListener('click', buscarFlor);
   document.getElementById('deletar').addEventListener('click', deletarFlor)
 });
 
-async function buscarFlor(){
-  if(db == undefined){
-    console.log("deu erro");
-  }
-  const tx = await db.transaction('foto', 'readonly');
-    const store = await tx.objectStore('foto');
-    const anotacoes = await store.getAll();
-    if(anotacoes){
-        const divLista = anotacoes.map(foto => {
-            return `<div class="item">
-                    <p>Anotação</p>
-                    <p>${foto.nome} - ${foto.camera} </p>
-                   </div>`;
-        });
-        listagem(divLista.join(' '));
-  }
-}
 
-async function cadastrarFlor () {
-  let camera = document.getElementById("camera").Value;
-  let nome = document.getElementById("nome").Value;
-  const tx = await db.transaction('foto', 'readwrite')
-  const store = tx.objectStore('foto');
-  try {
-    await store.add({camera: camera, nome: nome});
-    await tx.done;
-    limparCampos();
-    console.log('');
-  } catch (error) {
-    console.error('', error);
-    tx.abort();
-  }
-}
